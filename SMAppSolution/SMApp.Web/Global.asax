7b7f80c6968390045a82bf5e92c4e9f3db4dda71
sarfarajlastGithub <sarfarajlast@gmail.com>
2016-12-22 06:03:28 +0530

    This is first time commit as this is a default Application

---
+++ SMAppSolution/SMApp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SMApp.Web.MvcApplication" Language="C#" %>
---
+++ SMAppSolution/SMApp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SMApp.Web.Startup))]
+namespace SMApp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
